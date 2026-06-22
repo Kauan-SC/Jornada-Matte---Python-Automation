03fd0dea0a4ef37fb265085b4bd01f5459497fad
Kauan-SC <kauansc12345@gmail.com>
2026-06-22 11:15:58 -0300

    Ajusting Logger

--- core/logger.py
+++ core/logger.py
@@ -1,21 +1,13 @@
 import logging
-from pathlib import Path
+import sys
 
-# Create logger paste if it doesn't exist
-# log_dir = Path(__file__).parent.parent / "logs"
-# log_dir.mkdir(exist_ok=True)
-
-# Logging configuration
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(message)s",
     datefmt="%d/%m/%Y %H:%M:%S",
-    # handlers=[
-    #     logging.StreamHandler(),
-    #     logging.FileHandler(log_dir / "jornada_matte.log", encoding="utf-8")
-    # ]
+    handlers=[logging.StreamHandler(sys.stdout)],
+    force=True
 )
-
 
 def get_logger(name: str) -> logging.Logger:
     return logging.getLogger(name)
